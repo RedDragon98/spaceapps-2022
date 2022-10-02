4fee0042ff140723fec1aca3dd16c44d1015b60e
RedDragon98 <sritest@live.com>
2022-10-01 18:48:25 -0700

    cleanup

--- src/app.jsx
+++ src/app.jsx
@@ -2,10 +2,9 @@ import React, { useEffect, useRef } from "react";
 import { Canvas, useThree, useFrame } from '@react-three/fiber'
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-import { TextureLoader } from 'three/src/loaders/TextureLoader'
-import * as THREE from "three";
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { TextureLoader } from 'three/src/loaders/TextureLoader';
+import { useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 const CameraController = () => {
     const { camera, gl } = useThree();
@@ -24,17 +23,17 @@ const CameraController = () => {
     return null;
 };
 
-const Sphere = () => {
-    const colorMap = useLoader(TextureLoader, 'bg.png')
+const Earth = () => {
+    const colorMap = useLoader(TextureLoader, 'letters.png');
     
-    const boxRef = useRef();
+    const earthRef = useRef();
 
     useFrame(() => {
-        boxRef.current.rotation.y += 0.01;
+        earthRef.current.rotation.y += 0.01;
     });
 
     return (
-    <mesh position={[0, 0, 0]} rotation-x={Math.PI * 0.25} rotation-y={Math.PI * 0.25} ref={boxRef}>
+    <mesh position={[0, 0, 0]} rotation-x={Math.PI * 0.25} rotation-y={Math.PI * 0.25} ref={earthRef}>
         <sphereGeometry attach="geometry" args={[20, 16, 16]} />
         <meshStandardMaterial map={colorMap}/>
     </mesh>
@@ -43,17 +42,17 @@ const Sphere = () => {
   
 
 export const App = () => {
-    const gltf = useLoader(GLTFLoader, '/untitled.gltf');
+    const gltf = useLoader(GLTFLoader, '/iss.gltf');
 
     return (
         <Canvas>
             <CameraController />
             <ambientLight />
             <spotLight intensity={0.3} position={[5, 10, 50]} />
-            <Sphere/>
+            <Earth/>
             {/*<primitive object={new THREE.AxesHelper(10)} />*/}
             <mesh>
-            <primitive object={gltf.scene} position={[0, 28, 0]} scale={[0.2, 0.2, 0.2]} rotation={[0, 0, 90]}/>
+            <primitive object={gltf.scene} position={[0, 28, 0]} scale={[0.2, 0.2, 0.2]} rotation={[0, 0, 90]}/> {/* ISS */}
             </mesh>
         </Canvas>
     );
