9521dec495cfd3a86c5219a0b4d9819e6d5059ff
RedDragon98 <sritest@live.com>
2022-10-01 18:02:45 -0700

    add texture to earth

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,54 @@
-import React, { useRef, useState } from 'react';
-import { useFrame, useLoader } from '@react-three/fiber';
+import React, { useEffect } from "react";
+import { Canvas, useThree } from '@react-three/fiber'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import * as THREE from "three";
+import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
-export function ISS(props) {
-    const gltf = useLoader(GLTFLoader, '/untitled.gltf')
+const CameraController = () => {
+    const { camera, gl } = useThree();
+    useEffect(
+        () => {
+            const controls = new OrbitControls(camera, gl.domElement);
+
+            controls.minDistance = 20;
+            controls.maxDistance = 40;
+            return () => {
+                controls.dispose();
+            };
+        },
+        [camera, gl]
+    );
+    return null;
+};
+
+function Sphere(props) {
+    const colorMap = useLoader(TextureLoader, 'bg.png')
 
     return (
-        <mesh>
-            <primitive object={gltf.scene} />
-        </mesh>
-    )
+    <mesh position={[0, 0, 0]} castShadow>
+        <sphereGeometry attach="geometry" args={[20, 16, 16]} />
+        <meshStandardMaterial map={colorMap}/>
+    </mesh>
+    );
 }
+  
+
+export const App = () => {
+    const gltf = useLoader(GLTFLoader, '/untitled.gltf');
+
+    return (
+        <Canvas>
+            <CameraController />
+            <ambientLight />
+            <spotLight intensity={0.3} position={[5, 10, 50]} />
+            <Sphere/>
+            {/*<primitive object={new THREE.AxesHelper(10)} />*/}
+            <mesh>
+            <primitive object={gltf.scene} position={[0, 28, 0]} scale={[0.2, 0.2, 0.2]} rotation={[0, 0, 90]}/>
+            </mesh>
+        </Canvas>
+    );
+};
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import { ISS } from './app'
+import { App } from './app'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Canvas } from '@react-three/fiber'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <ISS position={[0, 0, 0]} />
-      </Canvas>,
-  </React.StrictMode>
+    <React.StrictMode>
+        <div style={{ position: "relative", width: window.innerWidth, height: window.innerHeight }}>
+            <App/>
+        </div>
+    </React.StrictMode>
 );
